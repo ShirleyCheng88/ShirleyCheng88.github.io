2895f8ab4a681574fc0e257c9267b836561e55c5
Shirley <e0878832@u.nus.edu>
2023-04-23 21:06:26 +0800

    I add the following function: 1.reach the set temperature after exactly x(now is 100) iterations, but with random increments at each step. 2.To clamp the input value to the allowed range(-20,200)

--- static/index.js
+++ static/index.js
@@ -37,9 +37,18 @@ function formatInputValue(inputElement) {
   document.getElementById('second-unit').addEventListener('blur', (event) => {
     formatInputValue(event.target);
   });
+
   // Get the input value and convert to a number
   let numberTyped = parseFloat(document.getElementById('main-unit').value);
-
+   // Clamp the input value between -20 and 200
+   if (numberTyped < -20) {
+    numberTyped = -20;
+    document.getElementById('main-unit').value = numberTyped.toFixed(2);
+  } else if (numberTyped > 200) {
+    numberTyped = 200;
+    document.getElementById('main-unit').value = numberTyped.toFixed(2);
+  }
+  
   if (mainUnit === 'celsius') {
     if (secondUnit === 'kelvin') {
       setTemp = numberTyped + 273.15;
@@ -71,11 +80,23 @@ function formatInputValue(inputElement) {
     clearTimeout(inputTimer);
   }
   
+  //add
+  let currentTyped = parseFloat(document.getElementById('second-unit').value);
+  // Clamp the input value between -20 and 200
+  if (currentTyped < -20) {
+    currentTyped = -20;
+    document.getElementById('second-unit').value = currentTyped.toFixed(2);
+  } else if (currentTyped > 200) {
+    currentTyped = 200;
+    document.getElementById('second-unit').value = currentTyped.toFixed(2);
+  }
+
   // Start a new timer to check for temperature changes
   inputTimer = setTimeout(() => {
     // Get the current temperature and convert to a number
     let currentTyped = parseFloat(document.getElementById('second-unit').value);
     
+
     // If the temperatures are different, start a loop to change the current temperature gradually
    if (currentTyped !== setTemp) {
       // Define variables for the loop
@@ -88,13 +109,28 @@ function formatInputValue(inputElement) {
       
       // Define a function to change the temperature and update the display
       function updateTemperature() {
-        console.log('Minus:', test); 
-        currentTyped += increment;
+      // Calculate the remaining steps
+      const remainingSteps = 100 - counter;
+      // Calculate the remaining temperature difference
+      const remainingDiff = setTemp - currentTyped;
+       // Generate a random number between -1 and 1, then multiply it by a scale factor
+      const scaleFactor = 2 * Math.abs(remainingDiff) / remainingSteps;
+      const randomIncrement = (Math.random() * 2 - 1) * scaleFactor;
+
+      // Calculate the new temperature by adding the random increment
+      const newTemp = currentTyped + randomIncrement;
+
+      // Check if the loop has run 30 times
+      if (counter >= 99) {
+       // Set the temperature to the exact setTemp value
+        document.getElementById('second-unit').value = setTemp.toFixed(2);
+      } else {
+        // Update the current temperature with the new value and continue the loop
+        currentTyped = newTemp;
         document.getElementById('second-unit').value = currentTyped.toFixed(2);
         counter++;
-        if (counter < 30) {
-          setTimeout(updateTemperature, delay);
-        }
+        setTimeout(updateTemperature, delay);
+      }
       }
       
       // Start the loop to change the temperature
